225e4a837a01a0a5d6df2309677f3039d1b63609
Lolo <manuel_fidalgo@hotmail.com>
2020-12-23 18:40:19 +0100

    include more functions

--- packages/botonic-react/src/components/buttonix.jsx
+++ packages/botonic-react/src/components/buttonix.jsx
@@ -1,3 +1,9 @@
 export function hello() {
-  console.log('hello')
+  console.log('hello buttonix')
+  console.log('how are you?')
+}
+
+export function bye() {
+  console.log('bye buttonix')
+  console.log('see ya!')
 }
