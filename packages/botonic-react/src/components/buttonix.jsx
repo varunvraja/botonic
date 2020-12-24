d4422f3d4405f8e9a5b324379f7c0f05237e9ffc
Lolo <manuel_fidalgo@hotmail.com>
2020-12-24 09:43:40 +0100

    include a branch

--- packages/botonic-react/src/components/buttonix.jsx
+++ packages/botonic-react/src/components/buttonix.jsx
@@ -1,6 +1,12 @@
 export function hello() {
   console.log('hello buttonix')
   console.log('how are you?')
+  const rand = Boolean(Math.round(Math.random()))
+  if (rand) {
+    console.log('true')
+  } else {
+    console.log('false')
+  }
 }
 
 export function bye() {
